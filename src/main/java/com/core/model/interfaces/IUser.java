3b7fee6d7531c71172a80bc504cdd718085367ef
João Gabriel <joao.conceicao@inmetrics.com.br>
2021-05-14 20:19:50 -0300

    Improve: Implment user interface in User class

--- src/main/java/com/core/model/interfaces/IUser.java
+++ src/main/java/com/core/model/interfaces/IUser.java
@@ -2,8 +2,8 @@ package com.core.model.interfaces;
 
 public interface IUser extends IBaseModel {
     public String getName();
-    public String getAge();
-    public void setName();
-    public void setAge();
+    public Integer getAge();
+    public void setName(String param);
+    public void setAge(Integer param);
     
 }
