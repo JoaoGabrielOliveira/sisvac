10e643f4375490245b436f6b03be2b159fab04a5
João Gabriel <joao.conceicao@inmetrics.com.br>
2021-05-14 19:19:01 -0300

    Add static class Extensions

---
+++ src/main/java/com/core/Extensions.java
@@ -0,0 +1,7 @@
+package com.core;
+
+public class Extensions {
+    public static String capitalize(String string){
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
+}
