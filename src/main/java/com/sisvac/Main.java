a945792067f8cd3fb03f4009206e20f280235152
Bruno Serafim <bruno_serafim08@hotmail.com>
2021-05-28 23:22:45 -0300

    adicionei uma variavel do tipo inteiro

--- src/main/java/com/sisvac/Main.java
+++ src/main/java/com/sisvac/Main.java
@@ -3,9 +3,13 @@ package com.sisvac;
 import com.core.database.Connector;
 import com.core.database.Operator;
 import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int num;
+        System.out.println("Hello World!!");
+        
+        num = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero"));
     }
 }
