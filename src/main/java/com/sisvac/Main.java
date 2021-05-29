e28a3c1ac81b4772e3af15449660c4813d793439
João Gabriel <joao.conceicao@inmetrics.com.br>
2021-05-28 22:54:30 -0300

    Adicionei hello world

--- src/main/java/com/sisvac/Main.java
+++ src/main/java/com/sisvac/Main.java
@@ -6,17 +6,6 @@ import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
-        Connector conn = new Connector("mysql://localhost", "3306", "loja", "root", "1234");
-        Operator oper = new Operator(conn);
-        
-        try{
-            ResultSet set = oper.list("usuarios");
-            while(set.next()){
-                System.out.print(set.getString(0));
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        System.out.println("Hello World");
     }
 }
