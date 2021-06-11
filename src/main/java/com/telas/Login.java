a8446d689bd99f47a0b1549938df5db7c6f7e0fd
Bruno Serafim <bruno_serafim08@hotmail.com>
2021-06-11 18:59:05 -0300

    Funcionalidades que o João pediu

--- src/main/java/com/telas/Login.java
+++ src/main/java/com/telas/Login.java
@@ -67,6 +67,7 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\User\\Documents\\sisvac\\src\\main\\java\\img\\icons\\logosisvac.png")); // NOI18N
         jLabel2.setText("jLabel2");
 
         Senha.setBorder(javax.swing.BorderFactory.createTitledBorder("Senha"));
