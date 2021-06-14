88ed63455c7b3e08517e36e57dece38ca554c96c
João Gabriel <joao.conceicao@inmetrics.com.br>
2021-06-14 08:59:09 -0300

    Adiconando classe Main

--- src/main/java/com/sisvac/Main.java
+++ src/main/java/com/sisvac/Main.java
@@ -1,14 +1,7 @@
 package com.sisvac;
 
-import com.core.database.Operator;
-import java.sql.ResultSet;
-import javax.swing.JOptionPane;
-
 public class Main {
-    public static void main(String[] args) {
-        int num;
-        System.out.println("Hello World!!");
-        
-        num = Integer.parseInt(JOptionPane.showInputDialog("Digite or numero"));
+    public static void main(String[] args) {   
+        new com.telas.Login().setVisible(true);
     }
 }
